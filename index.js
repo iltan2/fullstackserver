ee2a4b12aa2acc24d08cbf528b9eff56ace69934
Israel Tan <israeltan2@gmail.com>
2023-04-02 18:02:14 +0800

    Update up to exercise 3.16

--- index.js
+++ index.js
@@ -65,15 +65,44 @@ app.post("/api/persons", (request, response) => {
 
 });
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  .then(updatedPerson => response.json(updatedPerson))
+  .catch(error => next(error))
+}
+)
+
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 });
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware.
+app.use(errorHandler);
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
